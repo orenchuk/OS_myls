bb71c73b54c791f0747c103878c13a4543b1a2a7
Yevhenii Orenchuk <orenchuk98@gmail.com>
2018-05-17 17:08:41 +0300

    framework done

--- main.cpp
+++ main.cpp
@@ -26,21 +26,18 @@ void helpInfo() {
     cout << header << body << endl;
 }
 
-void showPathes(vector<string>& pathes) {
-    if (pathes.empty()) {
-        pathes.push_back(".");
-    }
-    for (auto& path : pathes) {
-        cout << "path: " << path << endl;
-    }
+vector<string> getFilesForPathes(const vector<string>& pathes) {
+    vector<string> files;
+    
+    return files;
 }
 
 void showFiles(const vector<string>& pathes) {
-    
+    cout << "showFiles" << endl;
 }
 
 void showFilesRecursively(const vector<string>& pathes) {
-    
+    cout << "showFilesRecursively" << endl;
 }
 
 void showFilesWithDetails(const vector<string>& pathes) {
@@ -55,48 +52,46 @@ void showFilesSorted(vector<string>& pathes, bool reversed) {
     cout << "Sorting function" << endl;
 }
 
-vector<string> getFilesForPathes(const vector<string>& pathes) {
-    vector<string> files;
-    
-    return files;
-}
-
 void readArgs(int argc, const char * argv[], vector<string>& pathes) {
+    vector<string> args;
     for (int i = 1; i < argc; ++i) {
         string argument = argv[i];
-        if ((argument == "-h") || (argument == "--help")) {
-            // TO-DO: design the way that function will parse args and only after it will execute them
-            helpInfo();
-            exit(0);
-        } else if (argument == "-l") {
-            showFilesWithDetails(pathes);
-        } else if (boost::contains(argument, "--sort")) {
-            bool reversed = false;
-            for (int j = i; j < argc; ++j) {
-                string temp = argv[j];
-                if (temp == "-r") {
-                    reversed = true;
-                }
-            }
-            showFilesSorted(pathes, reversed);
-        } else if (argument == "-F") {
-            showFilesWithSpecialChars(pathes);
-        } else if (argument == "-R") {
-            showFilesRecursively(pathes);
-        } else if (boost::contains(argument, "-")) {
-            cerr << "Error: there is no such option: " << argument << endl;
-            exit(-1);
+        if ((boost::contains(argument, "-")) || (boost::contains(argument, "--"))) {
+            args.push_back(argument);
         } else {
             pathes.push_back(argument);
         }
     }
     
-    showPathes(pathes);
+    for (int i = 0; i < args.size(); ++i) {
+        string argument = args[i];
+        if ((argument == "-h") || (argument == "--help")) {
+            helpInfo();
+            exit(0);
+        } else if (argument == "-l") {
+            showFilesWithDetails(pathes);
+        } else if (argument == "-F") {
+            showFilesWithSpecialChars(pathes);
+        } else if (argument == "-R") {
+            showFilesRecursively(pathes);
+        } else if (boost::contains(argument, "--sort")) {
+            bool reversed = false;
+            for (int j = i; j < args.size(); ++j) {
+                string temp = args[j];
+                if (temp == "-r") {
+                    reversed = true;
+                }
+            }
+            showFilesSorted(pathes, reversed);
+        } else if (boost::contains(argument, "-")) {
+            cerr << "Error: there is no such option: " << argument << endl;
+            exit(-1);
+        }
+    }
 }
 
 int main(int argc, const char * argv[]) {
     vector<string> pathes;
-    vector<string> files;
     readArgs(argc, argv, pathes);
     return 0;
 }
